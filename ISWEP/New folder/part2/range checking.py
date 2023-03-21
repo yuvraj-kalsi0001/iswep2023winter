0cdeb4ff8c08d9abc0bcb752501fc797a76701b3
yuvraj-kalsi0001 <97918056+yuvraj-kalsi0001@users.noreply.github.com>
2023-03-21 13:37:56 -0230

    c

---
+++ ISWEP/New folder/part2/range checking.py
@@ -0,0 +1,11 @@
+def range_subset(range1, range2):
+    """Whether range1 is a subset of range2."""
+    if not range1:
+        return True  # empty range is subset of anything
+    if not range2:
+        return False  # non-empty range can't be subset of empty range
+    if len(range1) > 1 and range1.step % range2.step:
+        return False  # must have a single value or integer multiple step
+    return range1.start in range2 and range1[-1] in range2
+
+print(range_subset(range(2140147,2140356), range(2140140, 2140357)))
