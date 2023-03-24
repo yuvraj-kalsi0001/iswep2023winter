2f25d7f8bc59299d8f8ca01166dc844fdcbf437d
yuvraj-kalsi0001 <97918056+yuvraj-kalsi0001@users.noreply.github.com>
2023-03-24 14:08:01 -0230

    c

---
+++ ISWEP/New folder/part2/GenomeCroping.py
@@ -0,0 +1,91 @@
+import csv
+
+# Open the CSV file
+with open('within_range.csv', newline='') as csvfile:
+
+    # Create a CSV reader object
+    reader = csv.reader(csvfile)
+
+    # Skip the header row
+    next(reader)
+
+    # Initialize variables to store column 5 and 6 values
+    prev_col5 = None
+    prev_col6 = None
+
+    # Initialize 2D list to store values
+    values = []
+
+    # Loop through the rows of the CSV file
+    for row in reader:
+
+        # Check if value in column 6 is the same as the previous row
+        if row[5] == prev_col6:
+            prev_col5 = row[4]
+            continue
+
+        # Add the value in column 4 to the last sublist of the 2D list
+        if values and row[5] == values[-1][1]:
+            # values[-1][0].append(row[3])
+            print(1)
+        else:
+            if len(values)!=0:
+                values[-1].append(prev_col5)
+            values.append([row[3], row[5]])
+
+        # Update the variable storing the previous value in column 6
+        prev_col6 = row[5]
+    values[-1].append(row[4])
+
+# Print the 2D list
+print(values)
+
+
+# read the GFF file and get the lines within the specified indices
+filename = "albidoflavus_J1074.gff"
+# start_index = 10
+# end_index = 20
+with open(filename) as f:
+    lines = f.readlines()
+
+# extract the lines after the row containing "##FASTA"
+fasta_index = None
+for i, line in enumerate(lines):
+    if "##FASTA" in line:
+        fasta_index = i
+        break
+if fasta_index is None:
+    print("No FASTA string found.")
+    exit()
+extracted_lines = lines[fasta_index+2:]
+
+# write the extracted lines to a CSV file with the heading
+# heading = ["Column1", "Column2", "Column3"]
+output_filename = "output.csv"
+with open(output_filename, mode='w', newline='') as f:
+    writer = csv.writer(f)
+    # writer.writerow(heading)
+    for line in extracted_lines:
+        writer.writerow(line.split('\t')[:3])  # write only the first three columns
+
+
+# read the CSV file
+with open('output.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    data = []
+    for row in csv_reader:
+        data.append(row[-1].strip())
+        # print(row[0:])
+    joined_data = "".join(data)
+    # print(joined_data)
+
+# create a new GFF file
+with open('output1.gff', 'w') as gff_file:
+    for i in range(len(values)):
+        # write the element at index 1 in the inner array
+        gff_file.write(str(values[i][1]) + '\n')
+        # write the stripped string from the CSV file
+        start = int(values[i][0])
+        end = int(values[i][2])
+        # print(len(joined_data))
+        gff_file.write(joined_data[start:end] + '\n')
--- ISWEP/New folder/part2/range checking.py
+++ ISWEP/New folder/part2/range checking.py
@@ -1,11 +1,95 @@
-def range_subset(range1, range2):
-    """Whether range1 is a subset of range2."""
-    if not range1:
-        return True  # empty range is subset of anything
-    if not range2:
-        return False  # non-empty range can't be subset of empty range
-    if len(range1) > 1 and range1.step % range2.step:
-        return False  # must have a single value or integer multiple step
-    return range1.start in range2 and range1[-1] in range2
+import csv
+import operator
 
-print(range_subset(range(2140147,2140356), range(2140140, 2140357)))
+# Open the first CSV file
+with open('albidoflavus_J1074.csv') as file1:
+    reader1 = csv.reader(file1)
+    header1 = next(reader1)  # Save header row
+
+    # Open the second CSV file
+    with open('albidoflavus_J1074-.csv') as file2:
+        reader2 = csv.reader(file2)
+        header2 = next(reader2)  # Save header row
+
+        # Add column headers to first file
+        header1.extend(['Type', 'Most similar known cluster'])
+        within_range_file = open('within_range.csv', 'w', newline='')
+        not_within_range_file = open('not_within_range.csv', 'w', newline='')
+        within_range_writer = csv.writer(within_range_file)
+        not_within_range_writer = csv.writer(not_within_range_file)
+        within_range_writer.writerow(header1)
+        not_within_range_writer.writerow(header1)
+
+        # Loop through rows in file1
+        for row1 in reader1:
+            # Get the numbers from columns 4 and 5
+            num1_col4 = float(row1[3])
+            num1_col5 = float(row1[4])
+
+            # Loop through rows in file2
+            for row2 in reader2:
+                # Get the numbers from columns 3 and 4
+                num2_col3 = float(row2[2])
+                num2_col4 = float(row2[3])
+
+                # Check if the numbers from file1 are within the range of file2
+                if num2_col3 <= num1_col4 <= num2_col4 and num2_col3 <= num1_col5 <= num2_col4:
+                    # Copy data from column 3 and 5 of file2 to column 6 of file1
+                    row1.extend([row2[1], row2[4]])
+                    within_range_writer.writerow(row1)
+                    break  # Stop looping through rows in file2
+            else:
+                # If the inner loop completed without finding a match, write the row to not_within_range.csv
+                not_within_range_writer.writerow(row1)
+            # Reset file2 to start from the beginning for the next row in file1
+            file2.seek(0)
+            next(reader2)  # Skip header row
+
+        # Close the output files
+        within_range_file.close()
+        not_within_range_file.close()
+
+# Sorting file
+# Open the CSV file for reading
+with open('within_range.csv', 'r') as infile:
+    reader = csv.reader(infile)
+
+    # Skip the header row
+    header = next(reader)
+
+    # Sort the rows by the fourth column
+    sorted_rows = sorted(filter(None, reader), key=lambda row: int(row[3]))
+
+# Open the CSV file for writing
+with open('within_range.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
+
+    # Write the header row to the output file
+    writer.writerow(header)
+
+    # Write the sorted rows to the output file, skipping any blank rows
+    for row in filter(None, sorted_rows):
+        writer.writerow(row)
+
+        # Open the CSV file for reading
+with open('not_within_range.csv', 'r') as infile:
+    reader = csv.reader(infile)
+
+    # Skip the header row
+    header = next(reader)
+
+    # Sort the rows by the fourth column
+    sorted_rows = sorted(filter(None, reader), key=lambda row: int(row[3]))
+
+# Open the CSV file for writing
+with open('not_within_range.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
+
+    # Write the header row to the output file
+    writer.writerow(header)
+
+    # Write the sorted rows to the output file, skipping any blank rows
+    for row in filter(None, sorted_rows):
+        writer.writerow(row)
+
+        
